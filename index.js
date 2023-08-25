60e7f67c50f0717c12bcf058530daa73b90d430a
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2023-08-25 13:10:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,53 @@ setInterval(() => {
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
-}, 1000);
+}, 1000);    
+
+var audio=new Audio('Message.mp3');
+const frm=document.querySelector('#frm');
+const ip=document.querySelector('#ip');
+
+frm.addEventListener('submit',(e)=>{
+   e.preventDefault();
+   const alarm_time=new Date(ip.value);
+   const cur_time=new Date();
+   if(alarm_time>=cur_time){
+    const tf=alarm_time.getTime()-cur_time.getTime();
+    a=setTimeout(()=>{
+        alert('Alarm Ringing!');
+        let i=0;
+        b=setInterval(()=>{
+            audio.play();i++;
+            if(i>=20){clearInterval(b);}
+        },1000);
+    },tf);
+   }
+});
+
+const sfrm=document.querySelector('#sfrm');
+const sip=document.querySelector('#sip');
+const h=document.querySelector('#h');
+
+sfrm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const time=new Date(sip.value);
+    const cur_time=new Date();
+    if(time>cur_time){
+          const divtimer=document.createElement('div');
+          divtimer.classList.add('divtmr');
+          h.append(divtimer);
+          a=setInterval(()=>{
+              const cur_time=new Date();
+              const dif=Math.floor((time.getTime()-cur_time.getTime())/1000);
+              const dfh=Math.floor(dif/3600);
+              const dfm=Math.floor((dif%3600)/60);
+              const dfs=(dif%60);
+              const divtmr=document.querySelector('.divtmr');
+              divtimer.innerText=`COUNT-DOWN-TIMER:     ${dfh} : ${dfm} : ${dfs}`;
+              if(timr==cur_time){divtmr.remove();clearInterval(a);alert('Timer Completed!');}
+          },1000);
+    }
+    else{
+        alert('No Count-Down-Timer!');
+    }
+});
